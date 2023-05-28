016e78b531d70a267d5dc0198bd4a3252bd0ea2d
Hong-In-Taek <callocation@naver.com>
2023-05-28 11:49:08 +0900

    upload

--- model/kobert.py
+++ model/kobert.py
@@ -12,7 +12,7 @@ from tqdm import tqdm, tqdm_notebook
 
 from kobert_tokenizer import KoBERTTokenizer
 
-
+from transformers import BertForSequenceClassification as BertModel
 tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 batch_size = 16
 sequence_length = 128
